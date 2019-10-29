2abbb63faee91df01d121a6a1436912bdf4b6173
renko4546 <55457527+renko4546@users.noreply.github.com>
2019-10-29 22:33:02 +0700

    Initial commit

---
+++ app/src/main/java/padila1ilham/com/a5170411054/DaftarActivity.kt
@@ -0,0 +1,12 @@
+package padila1ilham.com.a5170411054
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DaftarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar)
+    }
+}
---
+++ app/src/main/java/padila1ilham/com/a5170411054/MainActivity.kt
@@ -0,0 +1,25 @@
+package padila1ilham.com.a5170411054
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       //TextView daftar = (TextView)findViewByid(R.id.txtDaftar)
+        //daftar.setOnClickListener(new View.OnClickListener(){
+          //  public void onClick(View v){
+          //      Intent  i = new Intent(MainActivity.this,DaftarActivity.class)
+          //              startActivity(i);
+
+          //  }
+       // })
+    }
+}
